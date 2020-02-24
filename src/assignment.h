4f1c8ffadd1ad269a57d26de057e9023d32e8dfd
Venkataram Edavamadathil Sivaram <Veskimo123@gmail.com>
2020-02-24 18:21:07 +0000

    appending

--- src/assignment.h
+++ src/assignment.h
@@ -1,9 +1,29 @@
 #ifndef ASSIGNMENT_H_
 #define ASSIGNMENT_H_
 
+// C++ Standard Libraries
+#include <string>
+
+// Custom Built Libraries
+#include "var_stack.h"
+
 template <class T>
 class assignment {
+	std::string m_cached;
+public:
+	assignment(std::string = "");
 
+	void operator()(var_stack <T> &) const;
 };
+
+template <class T>
+assignment <T> ::assignment(std::string str) :
+	m_cached(str) {}
+
+template <class T>
+void assignment <T> ::operator()(var_stack <T> &vst) const
+{
+	
+}
 
 #endif
