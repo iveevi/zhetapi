6944ce35d371ca4c334434f3cebeece35290cae0
Venkataram Sivaram <veskimo123@gmail.com>
2020-04-27 11:46:36 -0700

    Organized application program
    The pipe directory has been moved to
    "temporary" build directory. Along with
    this, the flask application has been
    split up into related modules, making
    it cleaner as a whole.

--- web/application.py
+++ web/application.py
@@ -1,66 +1,4 @@
-import subprocess
-import os
-import signal
-import time
-import struct
-
-from subprocess import *
-from flask import Flask
-from flask import request
-from flask import render_template
-
-BUILD_DIR = "/home/ram/zhetapi/build/"
-PIPE_DIR = "/home/ram/null/"
-
-print("started....");
-fout = open(PIPE_DIR + "web2drv", "wb")
-
-print("[App]: Opened pipes...")
-
-def compute(input):
-    print("[App]: In computer")
-    print("[App]: Packaging " + input)
-
-    in_bytes = struct.pack("<I", len(input))
-    in_bytes += bytes(input, 'utf-8')
-
-    print("[App]: Sending " + str(bytearray(in_bytes)))
-    print("length is " + str(len(input)))
-
-    fout.write(bytearray(in_bytes))
-    fout.flush()
-
-    print("opening fin");
-    fin = open(PIPE_DIR + "drv2web", "r")
-    # print("fin opened")
-    # x = struct.unpack("<I", fin.read())[0]
-    # print("drv acked " + str(x) + " bytes")
-    x = fin.readline()
-    print("got: " + x)
-    fin.close()
-
-    return x
-
-compute("23 / 54")
-compute("e^2")
-# compute("")
-
-# fout.close()
-
-# Regular text to LaTeX converter
-def convert(input):
-    print("[App]: In converter")
-    sin = open("../build/texifier.in", "w")
-    sin.write(input)
-    sin.close()
-
-    subprocess.run(["./../build/texifier"])
-
-    sout = open("../build/texifier.out", "r")
-    text = sout.read()
-    sout.close()
-
-    return text
+import communicate
 
 from flask import Flask
 from flask import request
@@ -79,8 +17,8 @@ def show_calculate_view():
 @app.route('/calculate', methods=['POST'])
 def calculate_post():
     text = request.form['text']
-    out = compute(text)
-    text = convert(text)
+    out = communicate.compute(text)
+    text = communicate.convert(text)
     return render_template('calculate.html', input = text, out = out)
 
 logs = []
@@ -92,10 +30,7 @@ def show_session_view():
 @app.route('/session', methods=['POST'])
 def session_post():
     text = request.form['text']
-    out = compute(text)
-    text = convert(text)
-    
+    out = communicate.compute(text)
+    text = communicate.convert(text)
     logs.insert(0, text + " = \\textbf{" + out + "}");
     return render_template('session.html', logs = logs)
-
-driver.wait()
---
+++ web/communicate.py
@@ -0,0 +1,35 @@
+import struct
+import subprocess
+
+PIPE_DIR = "/home/ram/zhetapi/build/"
+
+fout = open(PIPE_DIR + "web2drv", "wb")
+
+# Regular text to Zhetapi Engine
+def compute(input):
+    in_bytes = struct.pack("<I", len(input))
+    in_bytes += bytes(input, 'utf-8')
+
+    fout.write(bytearray(in_bytes))
+    fout.flush()
+
+    fin = open(PIPE_DIR + "drv2web", "r")
+
+    x = fin.readline()
+    fin.close()
+
+    return x
+
+# Regular text to LaTeX converter
+def convert(input):
+    sin = open("../build/texifier.in", "w")
+    sin.write(input)
+    sin.close()
+
+    subprocess.run(["./../build/texifier"])
+
+    sout = open("../build/texifier.out", "r")
+    text = sout.read()
+    sout.close()
+
+    return text
