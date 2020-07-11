7a74f8124b09a955fa83cb184c077eb992f84f53
Venkataram Sivaram <veskimo123@gmail.com>
2020-07-10 21:05:05 -0700

    Clean-up and preperations

--- engine/barn.h
+++ engine/barn.h
@@ -1,91 +1,180 @@
 #ifndef BARN_H_
 #define BARN_H_
 
+#include <cmath>
+#include <functional>
 #include <iostream>
 #include <string>
-#include <typeinfo>
-#include <typeindex>
 #include <tuple>
-#include <functional>
+#include <typeindex>
+#include <typeinfo>
 
-#include "vtable.h"
-#include "rational.h"
 #include "element.h"
 #include "matrix.h"
-#include "zcomplex.h"
-#include "token.h"
 #include "operand.h"
 #include "operation.h"
+#include "rational.h"
+#include "token.h"
+#include "vtable.h"
+#include "zcomplex.h"
 
-#define ADD_BINARY_OP(str, A, B, O)						\
-	ops.push_back({{std::string(#str), std::vector <std::type_index>			\
-			{typeid(operand <A>), typeid(operand <B>)}},		\
-		new operation <O> {std::string(#str), "$1 (" + std::string(#A) + ") " + std::string(#str) + " $2 (" + std::string(#B) + ")", 2, \
-			[&](const std::vector <token *> &ins) {						\
-				operand <A> *a = dynamic_cast <operand <A> *> (ins[0]);				\
-				operand <B> *b = dynamic_cast <operand <B> *> (ins[1]);				\
-													\
-				return new operand <O> (a->get() str b->get());			\
-			}										\
-	}});
+#define AB_OP(str, A, B, O)									\
+	ops.push_back({										\
+			{									\
+				std::string(#str),						\
+				std::vector <std::type_index> {					\
+					typeid(operand <A>),					\
+					typeid(operand <B>)					\
+				}								\
+			},									\
+												\
+			new operation <O> {							\
+				std::string(#str),						\
+				"$1 (" + std::string(#A) + ") " + std::string(#str)		\
+					+ " $2 (" + std::string(#B) + ")",			\
+				2,								\
+				[&](const std::vector <token *> &ins) {				\
+					operand <A> *a = dynamic_cast <operand <A> *> (ins[0]);	\
+					operand <B> *b = dynamic_cast <operand <B> *> (ins[1]);	\
+												\
+					return new operand <O> (a->get() str b->get());		\
+				}								\
+			}									\
+	});
 
-#define ADD_BINARY_OP_FTR(str, A, B, O, ftr)						\
-	ops.push_back({{std::string(#str), std::vector <std::type_index>			\
-			{typeid(operand <A>), typeid(operand <B>)}},		\
-		new operation <O> {std::string(#str), "$1 (" + std::string(#A) + ") " + std::string(#str) + " $2 (" + std::string(#B) + ")", 2, \
-			[&](const std::vector <token *> &ins) {						\
-				operand <A> *a = dynamic_cast <operand <A> *> (ins[0]);				\
-				operand <B> *b = dynamic_cast <operand <B> *> (ins[1]);				\
-													\
-				return new operand <O> (ftr);			\
-			}										\
-	}});
+#define ABH_OP(str, A, B, O)									\
+	AB_OP(str, A, B, O)									\
+	AB_OP(str, B, A, O)
 
-#define ADD_HETER_BINARY_OP_FTR(str, A, B, O, ftr)					\
-	ops.push_back({{std::string(#str), std::vector <std::type_index>			\
-			{typeid(operand <A>), typeid(operand <B>)}},		\
-		new operation <O> {std::string(#str), "$1 (" + std::string(#A) + ") " + std::string(#str) + " $2 (" + std::string(#B) + ")", 2, \
-			[&](const std::vector <token *> &ins) {						\
-				operand <A> *a = dynamic_cast <operand <A> *> (ins[0]);				\
-				operand <B> *b = dynamic_cast <operand <B> *> (ins[1]);				\
-													\
-				return new operand <O> (ftr);			\
-			}										\
-	}});									\
-											\
-	ops.push_back({{std::string(#str), std::vector <std::type_index>			\
-			{typeid(operand <B>), typeid(operand <A>)}},		\
-		new operation <O> {std::string(#str), "$1 (" + std::string(#B) + ") " + std::string(#str) + " $2 (" + std::string(#A) + ")", 2, \
-			[&](const std::vector <token *> &ins) {						\
-				operand <A> *a = dynamic_cast <operand <A> *> (ins[1]);				\
-				operand <B> *b = dynamic_cast <operand <B> *> (ins[0]);				\
-													\
-				return new operand <O> (ftr);			\
-			}										\
-	}});
+#define AB_OP_FTR(str, A, B, O, ftr)								\
+	ops.push_back({										\
+			{									\
+				std::string(#str),						\
+				std::vector <std::type_index> {					\
+					typeid(operand <A>),					\
+					typeid(operand <B>)					\
+				}								\
+			},									\
+												\
+			new operation <O> {							\
+				std::string(#str),						\
+				"$1 (" + std::string(#A) + ") " + std::string(#str)		\
+					+ " $2 (" + std::string(#B) + ")",			\
+				2,								\
+				[&](const std::vector <token *> &ins) {				\
+					operand <A> *a = dynamic_cast <operand <A> *> (ins[0]);	\
+					operand <B> *b = dynamic_cast <operand <B> *> (ins[1]);	\
+												\
+					return new operand <O> (ftr);				\
+				}								\
+			}									\
+	});
 
-#define ADD_HETER_BINARY_OP(str, A, B, O)	\
-	ADD_BINARY_OP(str, A, B, O)		\
-	ADD_BINARY_OP(str, B, A, O)
+#define ABH_OP_FTR(str, A, B, O, ftr)								\
+	ops.push_back({										\
+			{									\
+				std::string(#str),						\
+				std::vector <std::type_index> {					\
+					typeid(operand <A>),					\
+					typeid(operand <B>)					\
+				}								\
+			},									\
+												\
+			new operation <O> {							\
+				std::string(#str),						\
+				"$1 (" + std::string(#A) + ") " + std::string(#str)		\
+					+ " $2 (" + std::string(#B) + ")",			\
+				2,								\
+				[&](const std::vector <token *> &ins) {				\
+					operand <A> *a = dynamic_cast <operand <A> *> (ins[0]);	\
+					operand <B> *b = dynamic_cast <operand <B> *> (ins[1]);	\
+												\
+					return new operand <O> (ftr);				\
+				}								\
+			}									\
+	});											\
+												\
+	ops.push_back({										\
+			{									\
+				std::string(#str),						\
+				std::vector <std::type_index> {					\
+					typeid(operand <B>),					\
+					typeid(operand <A>)					\
+				}								\
+			},									\
+												\
+			new operation <O> {							\
+				std::string(#str),						\
+				"$1 (" + std::string(#B) + ") " + std::string(#str)		\
+					+ " $2 (" + std::string(#A) + ")",			\
+				2,								\
+				[&](const std::vector <token *> &ins) {				\
+					operand <A> *a = dynamic_cast <operand <A> *> (ins[1]);	\
+					operand <B> *b = dynamic_cast <operand <B> *> (ins[0]);	\
+												\
+					return new operand <O> (ftr);				\
+				}								\
+			}									\
+	});
 
-#define ADD_BINARY_OP_SET(str)											\
-	ADD_BINARY_OP(str, Z, Z, Z);										\
-	ADD_BINARY_OP(str, R, R, R);										\
-	ADD_BINARY_OP(str, Q, Q, Q);										\
-	ADD_BINARY_OP(str, CR, CR, CR);										\
-	ADD_BINARY_OP(str, CQ, CQ, CQ);										\
-														\
-	ADD_HETER_BINARY_OP(str, R, Z, R);									\
-	ADD_HETER_BINARY_OP_FTR(str, Z, Q, Q, Q(a->get()) str b->get());					\
-	ADD_HETER_BINARY_OP(str, R, Q, R);									\
-	ADD_HETER_BINARY_OP(str, R, CR, CR);									\
-	ADD_HETER_BINARY_OP_FTR(str, R, CQ, CR, CR(a->get() str R(b->get().real()), R(b->get().imag())));	\
-	ADD_HETER_BINARY_OP_FTR(str, Z, CR, CR, CR(a->get() str b->get().real(), b->get().imag()));		\
-	ADD_HETER_BINARY_OP_FTR(str, Z, CQ, CQ, CQ(a->get() str b->get().real(), b->get().imag()));		\
-	ADD_HETER_BINARY_OP_FTR(str, Q, CR, CR, CR(R(a->get()) str b->get().real(), b->get().imag()));		\
-	ADD_HETER_BINARY_OP_FTR(str, Q, CQ, CQ, CQ(a->get() str b->get().real(), b->get().imag()));             \
-	ADD_HETER_BINARY_OP_FTR(str, CR, CQ, CR, CR(a->get().real() str (R) b->get().real(),                    \
+#define AB_OP_SET(str)										\
+	AB_OP(str, Z, Z, Z);									\
+	AB_OP(str, R, R, R);									\
+	AB_OP(str, Q, Q, Q);									\
+	AB_OP(str, CR, CR, CR);									\
+	AB_OP(str, CQ, CQ, CQ);									\
+												\
+	ABH_OP(str, R, Z, R);									\
+	ABH_OP_FTR(str, Z, Q, Q, Q(a->get()) str b->get());					\
+	ABH_OP(str, R, Q, R);									\
+	ABH_OP(str, R, CR, CR);									\
+	ABH_OP_FTR(str, R, CQ, CR, CR(a->get() str R(b->get().real()), R(b->get().imag())));	\
+	ABH_OP_FTR(str, Z, CR, CR, CR(a->get() str b->get().real(), b->get().imag()));		\
+	ABH_OP_FTR(str, Z, CQ, CQ, CQ(a->get() str b->get().real(), b->get().imag()));		\
+	ABH_OP_FTR(str, Q, CR, CR, CR(R(a->get()) str b->get().real(), b->get().imag()));	\
+	ABH_OP_FTR(str, Q, CQ, CQ, CQ(a->get() str b->get().real(), b->get().imag()));		\
+	ABH_OP_FTR(str, CR, CQ, CR, CR(a->get().real() str (R) b->get().real(),			\
 		a->get().imag() str (R) b->get().imag()));
+
+#define AU_OP(str, I, O)									\
+	ops.push_back({										\
+			{									\
+				std::string(#str),						\
+				std::vector <std::type_index> {typeid(operand <I>)}		\
+			},									\
+												\
+			new operation <O> {							\
+				std::string(#str),						\
+				"$1 (" + std::string(#I) + ")",					\
+				1, 								\
+				[&](const std::vector <token *> &ins) {				\
+					operand <I> *in = dynamic_cast				\
+						<operand <I> *> (ins[0]);			\
+												\
+					return new operand <O> (str(in->get()));		\
+				}								\
+			}									\
+	});
+
+#define AU_OP_FTR(str, I, O, ftr)								\
+	ops.push_back({										\
+			{									\
+				std::string(#str),						\
+				std::vector <std::type_index> {typeid(operand <I>)}		\
+			},									\
+												\
+			new operation <O> {							\
+				std::string(#str),						\
+				"$1 (" + std::string(#I) + ")",					\
+				1, 								\
+				[&](const std::vector <token *> &ins) {				\
+					operand <I> *in = dynamic_cast				\
+						<operand <I> *> (ins[0]);			\
+												\
+					return new operand <O> (ftr);				\
+				}								\
+			}									\
+	});
 
 /**
  * @brief Represents the working
@@ -164,48 +253,61 @@ barn <R, Z> ::barn() : z(), r(), q(), rc(), qc(), rm(), qm()
 	//////////////////////////////////////////
 	
 	// Addition and Subtraction
-	ADD_BINARY_OP_SET(+);
-	ADD_BINARY_OP_SET(-);
+	AB_OP_SET(+);
+	AB_OP_SET(-);
 
 	// Multiplication
-	ADD_BINARY_OP(*, Z, Z, Z);
-	ADD_BINARY_OP(*, R, R, R);
-	ADD_BINARY_OP(*, Q, Q, Q);
-	ADD_BINARY_OP(*, CR, CR, CR);
-	ADD_BINARY_OP(*, CQ, CQ, CQ);
+	AB_OP(*, Z, Z, Z);
+	AB_OP(*, R, R, R);
+	AB_OP(*, Q, Q, Q);
+	AB_OP(*, CR, CR, CR);
+	AB_OP(*, CQ, CQ, CQ);
 
-	ADD_HETER_BINARY_OP(*, R, Z, R);
-	ADD_HETER_BINARY_OP(*, Z, Q, Q);
-	ADD_HETER_BINARY_OP_FTR(*, R, Q, R, a->get() * (R) b->get());
-	ADD_HETER_BINARY_OP(*, R, CR, CR);
-	ADD_HETER_BINARY_OP_FTR(*, R, CQ, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
-	ADD_HETER_BINARY_OP_FTR(*, Z, CR, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
-	ADD_HETER_BINARY_OP_FTR(*, Z, CQ, CQ, CQ((Q) a->get() * b->get().real(), (Q) a->get() * b->get().imag()));
-	ADD_HETER_BINARY_OP_FTR(*, Q, CR, CR, CR((R) a->get() * b->get().real(), (R) a->get() * b->get().imag()));
-	ADD_HETER_BINARY_OP(*, Q, CQ, CQ);
+	ABH_OP(*, R, Z, R);
+	ABH_OP(*, Z, Q, Q);
+	ABH_OP_FTR(*, R, Q, R, a->get() * (R) b->get());
+	ABH_OP(*, R, CR, CR);
+	ABH_OP_FTR(*, R, CQ, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
+	ABH_OP_FTR(*, Z, CR, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
+	ABH_OP_FTR(*, Z, CQ, CQ, CQ((Q) a->get() * b->get().real(), (Q) a->get() * b->get().imag()));
+	ABH_OP_FTR(*, Q, CR, CR, CR((R) a->get() * b->get().real(), (R) a->get() * b->get().imag()));
+	ABH_OP(*, Q, CQ, CQ);
 
 	// Division
-	ADD_BINARY_OP(/, R, R, R);
-	ADD_BINARY_OP(/, Q, Q, Q);
-	ADD_BINARY_OP(/, CR, CR, CR);
-	ADD_BINARY_OP(/, CQ, CQ, CQ);
+	AB_OP(/, R, R, R);
+	AB_OP(/, Q, Q, Q);
+	AB_OP(/, CR, CR, CR);
+	AB_OP(/, CQ, CQ, CQ);
 
-	ADD_HETER_BINARY_OP(/, R, Z, R);
-	ADD_HETER_BINARY_OP(/, Z, Q, Q);
-	ADD_HETER_BINARY_OP_FTR(/, R, Q, R, a->get() * (R) b->get());
-	ADD_HETER_BINARY_OP(/, R, CR, CR);
-	ADD_HETER_BINARY_OP_FTR(/, R, CQ, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
-	ADD_HETER_BINARY_OP_FTR(/, Z, CR, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
-	ADD_HETER_BINARY_OP_FTR(/, Z, CQ, CQ, CQ((Q) a->get() * b->get().real(), (Q) a->get() * b->get().imag()));
-	ADD_HETER_BINARY_OP_FTR(/, Q, CR, CR, CR((R) a->get() * b->get().real(), (R) a->get() * b->get().imag()));
-	ADD_HETER_BINARY_OP(/, Q, CQ, CQ);
+	ABH_OP(/, R, Z, R);
+	ABH_OP(/, Z, Q, Q);
+	ABH_OP_FTR(/, R, Q, R, a->get() * (R) b->get());
+	ABH_OP(/, R, CR, CR);
+	ABH_OP_FTR(/, R, CQ, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
+	ABH_OP_FTR(/, Z, CR, CR, CR(a->get() * (R) b->get().real(), a->get() * (R) b->get().imag()));
+	ABH_OP_FTR(/, Z, CQ, CQ, CQ((Q) a->get() * b->get().real(), (Q) a->get() * b->get().imag()));
+	ABH_OP_FTR(/, Q, CR, CR, CR((R) a->get() * b->get().real(), (R) a->get() * b->get().imag()));
+	ABH_OP(/, Q, CQ, CQ);
 
 	// Exponentiation
-	ADD_BINARY_OP_FTR(^, Z, Z, Z, (Z) pow(a->get(), b->get()));
-	ADD_BINARY_OP_FTR(^, R, R, R, pow(a->get(), b->get()));
-	ADD_BINARY_OP_FTR(^, Q, Q, R, pow((R) a->get(), (R) b->get()));
-	ADD_BINARY_OP_FTR(^, CR, CR, CR, 1);
-	ADD_BINARY_OP_FTR(^, CQ, CQ, CQ, Q(1));
+	AB_OP_FTR(^, Z, Z, Z, (Z) pow(a->get(), b->get()));
+	AB_OP_FTR(^, R, R, R, pow(a->get(), b->get()));
+	AB_OP_FTR(^, Q, Q, R, pow((R) a->get(), (R) b->get()));
+	AB_OP_FTR(^, CR, CR, CR, 1);
+	AB_OP_FTR(^, CQ, CQ, CQ, Q(1));
+
+	// Trigonometry
+	AU_OP(std::sin, Z, R);
+	AU_OP_FTR(std::sin, Q, R, sin(R (in->get())));
+	AU_OP(std::sin, R, R);
+	
+	AU_OP(std::cos, Z, R);
+	AU_OP_FTR(std::sin, Q, R, cos(R (in->get())));
+	AU_OP(std::cos, R, R);
+	
+	AU_OP(std::tan, Z, R);
+	AU_OP_FTR(std::sin, Q, R, tan(R (in->get())));
+	AU_OP(std::tan, R, R);
 }
 
 template <class R, class Z>
