4281cdaa784c3720fe08fb7ce185c9c799900a23
Venkataram Sivaram <veskimo123@gmail.com>
2020-09-15 19:18:32 -0700

    Began CLI work

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,100 +1,26 @@
-#include <vector>
-#include <sstream>
+// C/C++ headers
 #include <iostream>
-#include <fstream>
+#include <string>
+
+// Engine headers
+#include <barn.hpp>
+#include <expression.hpp>
 
 using namespace std;
 
-enum type {
-	t_ident,
-	t_equals,
-	t_expr
-};
-
-std::string strtypes[] {
-	"ident",
-	"equals",
-	"expr"
-};
-
-struct token {
-	type kind;
-	std::string name;
-};
-
-token code(const std::string &str)
-{
-	if (str == "=")
-		return {t_equals, ""};
-
-	return {t_ident, str};
-}
-
-std::ostream &operator<<(std::ostream &os, const token &tok)
-{
-	std::string out = "[" + std::string(strtypes[tok.kind]) + "]";
-
-	if (tok.kind == t_ident)
-		out += " = " + tok.name;
-
-	os << out;
-
-	return os;
-}
-	
-void parse_file(ifstream &fin)
-{
-	std::vector <std::string> tokens;
-	std::string str;
-
-	token t;
-
-	char c;
-	
-	std::string line;
-	while (std::getline(fin, line)) {
-		std::istringstream iss(line);
-
-		tokens.clear();
-		str.clear();
-
-		while (iss.get(c)) {
-			if (isspace(c)) {
-				if (!str.empty()) {
-					tokens.push_back(str);
-					str.clear();
-				}
-			} else {
-				switch (c) {
-				case '=': case '(':
-				case ')': case ',':
-					if (!str.empty()) {
-						tokens.push_back(str);
-						str.clear();
-					}
-
-					tokens.push_back(std::string(1, c));
-					break;
-				default:
-					str += c;
-					break;
-				}
-			}
-		}
-
-		if (!str.empty())
-			tokens.push_back(str);
-
-		for (std::string str : tokens)
-			cout << "str: " << str << endl;
-
-		cout << "-----------------------------" << endl;
-	}
-}
-
 int main()
 {
-	ifstream fin("cli/sample");
+	zhetapi::Barn <double, int> barn;
 
-	parse_file(fin);
+	string line;
+	while (true) {
+		cout << "(zhetapi-cli) ";
+
+		getline(cin, line);
+
+		if (line == "q" || line == "quit")
+			return 0;
+
+		cout << "\t" << zhetapi::expr_str <double, int> (line) << endl;
+	}
 }
