9049f9d5ef1293391741d95294ffbd9d39ba4a09
Venkataram Edavamadathil Sivaram <Veskimo123@gmail.com>
2020-09-21 19:46:28 +0000

    Benchmarks for expanding functions

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,14 +1,110 @@
 // C/C++ headers
+#include <ctime>
+#include <cmath>
 #include <iostream>
 
 #include <dlfcn.h>
 
+// Engine headers
+#include <function.hpp>
+
+#define TEST1
+
 using namespace std;
 
-const char file[] = "tests/fctn.so";
+
+#if defined(TEST1)
+
+	const size_t rounds = 10000;
+
+	time_t startt;
+	time_t endt;
+
+	zhetapi::Barn <double, int> barn;
+
+	zhetapi::token *__fx_general(double x)
+	{
+		zhetapi::token *i1 = new zhetapi::operand <double> (x);
+
+		zhetapi::token *c1 = new zhetapi::operand <double> (54);
+
+		zhetapi::token *int1 = barn.compute("*", {i1, c1});
+		zhetapi::token *int2 = barn.compute("ln", {i1});
+
+		return barn.compute("+", {int1, int2});
+	}
+
+	double __fx_in_R_out_R(double x)
+	{
+		return 54.0 * x + log(x);
+	}
+
+	inline double __fx_in_R_out_R_inline(double x)
+	{
+		return 54.0 * x + log(x);
+	}
+
+#endif
 
 int main()
 {
+
+#if defined(TEST1)
+
+	zhetapi::Function <double, int> fx = "f(x) = x * 54 + ln(x)";
+
+	/////////////////////////////////////////////////////////////
+	cout << "Running with zhetapi::Function:" << endl;
+
+	startt = clock();
+
+	for (size_t i = 0; i < rounds; i++)
+		fx(10);
+
+	endt = clock();
+
+	cout << "\tTime: " << (endt - startt)/((double) CLOCKS_PER_SEC) << " seconds" << endl;
+
+	/////////////////////////////////////////////////////////////
+	cout << "Running with general function:" << endl;
+
+	startt = clock();
+
+	for (size_t i = 0; i < rounds; i++)
+		__fx_general(10);
+
+	endt = clock();
+
+	cout << "\tTime: " << (endt - startt)/((double) CLOCKS_PER_SEC) << " seconds" << endl;
+
+	/////////////////////////////////////////////////////////////
+	cout << "Running with specific function:" << endl;
+
+	startt = clock();
+
+	for (size_t i = 0; i < rounds; i++)
+		__fx_in_R_out_R(10);
+
+	endt = clock();
+
+	cout << "\tTime: " << (endt - startt)/((double) CLOCKS_PER_SEC) << " seconds" << endl;
+
+	/////////////////////////////////////////////////////////////
+	cout << "Running with inlined specific function:" << endl;
+
+	startt = clock();
+
+	for (size_t i = 0; i < rounds; i++)
+		__fx_in_R_out_R_inline(10);
+
+	endt = clock();
+
+	cout << "\tTime: " << (endt - startt)/((double) CLOCKS_PER_SEC) << " seconds" << endl;
+
+#elif defined(TEST2)
+
+	const char file[] = "tests/fctn.so";
+
 	cout << 24 << endl;
 
 	system("g++ tests/fctn.cpp -o tests/fctn.so -shared -fPIC");
@@ -26,6 +122,7 @@ int main()
 	cout << "f: " << f << endl;
 
 	const char *dlsym_error = dlerror();
+	
 	if (dlsym_error) {
 		cerr << "Cannot load symbol 'F': " << dlsym_error << '\n';
 		
@@ -37,4 +134,7 @@ int main()
 	cout << (*f)(24) << endl;
 
 	dlclose(handle);
+
+#endif
+
 }
