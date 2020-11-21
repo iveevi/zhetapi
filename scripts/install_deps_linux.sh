657bc2d2b3c6cbb3009513a11ca204b0b0d7d04c
Venkster123 <veskimo123@gmail.com>
2020-11-20 21:31:31 -0800

    Fixed travis issue with cmake in linux

--- scripts/install_deps_linux.sh
+++ scripts/install_deps_linux.sh
@@ -1,9 +1,3 @@
-mkdir $HOME/usr
-export PATH="$HOME/usr/bin:$PATH"
-wget https://cmake.org/files/v3.14/cmake-3.14.2-Linux-x86_64.sh
-chmod +x cmake-3.14.2-Linux-x86_64.sh
-./cmake-3.14.2-Linux-x86_64.sh --prefix=$HOME/usr --exclude-subdir --skip-license
-
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install libboost-dev gcc-8 g++-8 tree
