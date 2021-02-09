0b72a645432f19ce1e70a1f9ee3ac8850e2c9e9e
Venkataram Edavamadathil Sivaram <42660556+vedavamadathil@users.noreply.github.com>
2021-02-09 14:05:58 -0800

    Update install_deps_linux.sh

--- scripts/install_deps_linux.sh
+++ scripts/install_deps_linux.sh
@@ -1,3 +1,3 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get install libboost-dev gcc-8 g++-8 tree
+sudo apt-get update -y
+sudo apt-get install -y libboost-dev tree
