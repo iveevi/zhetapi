9bf1dfbd833ce24ca5c465ea7011e0cac6728fe5
Venkataram Sivaram <veskimo123@gmail.com>
2021-05-02 13:00:50 -0700

    Benchmarking current rvalue performance

--- experimental/exp.cpp
+++ experimental/exp.cpp
@@ -1,22 +1,101 @@
-#include <engine.hpp>
-#include <lang/error_handling.hpp>
+#include <chrono>
+#include <vector>
+#include <unordered_map>
 
+// Engine headers
+#include <engine.hpp>
+
+#include <core/rvalue.hpp>
+
+// Namespaces
 using namespace std;
 using namespace zhetapi;
 
+// Typedefs
+using hrclk = chrono::high_resolution_clock;
+
+// Timers
+hrclk::time_point start_t;
+hrclk::time_point end_t;
+
+const int ITERS = 10000000;
+
+// New struct
+struct Scope {
+        // Memory map (virtual "MMU")
+        unordered_map <string, size_t> mmu;
+
+        // Virtualized "memory"
+        vector <Token *> mem;
+
+        // Upper scope
+        // Scope *next;
+
+        void put(const string &str, Token *tptr) {
+                if (mmu.find(str) == mmu.end()) {
+                        // not yet mapped
+                        mmu[str] = mem.size();
+
+                        mem.push_back(tptr);
+                } else {
+                        // Consider deleting old stuff
+                        mem[mmu[str]] = tptr;
+                }
+        }
+
+        Token *operator[](size_t i) const {
+                return mem[i];
+        }
+};
+
+struct xvalue {
+        string sym;
+        size_t cin;     // Cached index
+        size_t off;     // Scope offset
+
+        xvalue(const string &str, Scope *scope) : sym(str) {
+                // Ignore null scopes for now
+                cin = scope->mmu[str];
+        }
+
+        Token *get(Scope *scope) const {
+                return scope->mem[cin];
+        }
+};
+
 int main()
 {
-	cout << levenshtein("sitting", "kitten") << endl;
+        Engine *engine = new Engine();
 
-	Engine *engine = new Engine(true);
+        engine->put("x", new Operand <int> (10));
+        engine->put("squared", new Operand <int> (10));
+        engine->put("foo", new Operand <int> (10));
+        engine->put("bar", new Operand <int> (10));
 
-	cout << "syms:" << endl;
-	for (auto str : engine->symbol_list())
-		cout << "\t" << str << endl;
-	
-	cout << "suggestions:" << endl;
-	for (auto str : symbol_suggestions("nill", engine->symbol_list()))
-		cout << "\t" << str << endl;
-	
-	symbol_error_msg("nill", engine);
+        start_t = hrclk::now();
+
+        rvalue rv("squared");
+        for (size_t i = 0; i < ITERS; i++)
+                rv.get(engine);
+        
+        end_t = hrclk::now();
+
+        cout << "Current time is: " << (chrono::duration_cast <chrono::milliseconds> (end_t - start_t)).count() << " ms" << endl;
+
+        Scope *scope = new Scope;
+
+        scope->put("x", new Operand <int> (10));
+        scope->put("squared", new Operand <int> (10));
+        scope->put("foo", new Operand <int> (10));
+        scope->put("bar", new Operand <int> (10));
+
+        start_t = hrclk::now();
+
+        xvalue xv("squared", scope);
+        for (size_t i = 0; i < ITERS; i++)
+                xv.get(scope);
+        
+        end_t = hrclk::now();
+
+        cout << "New time is: " << (chrono::duration_cast <chrono::milliseconds> (end_t - start_t)).count() << " ms" << endl;
 }
