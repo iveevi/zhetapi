c523ef76618cd509061fbfdf3bfee99b7396b7bd
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2021-05-18 23:55:27 +0000

    Correcting project version

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -7,7 +7,7 @@ if (${CMAKE_BUILD_TYPE} MATCHES Debug)
 elseif (${CMAKE_BUILD_TYPE} MATCHES Warn)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 elseif (${CMAKE_BUILD_TYPE} MATCHES Codecov)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0 --coverage")
 endif ()
 
 # Replace extensions
--- engine/core/set.hpp
+++ engine/core/set.hpp
@@ -1,21 +1,64 @@
 #ifndef SET_H_
 #define SET_H_
 
+// C/C++ headers
+#include <function>
+#include <string>
+#include <utility>
+
 // Engine headers
 #include "../token.hpp"
 
 namespace zhetapi {
 
 class Set : public Token {
+public:
+	using Permit = std::function <bool (Token *)>;
+private:
+	std::string	_name	= "";
+	Permit		_permit;
+public:
+	Set(const std::string &);
+	Set(const std::string &, Permit);
 
+	virtual bool present(Token *) const;
+
+	// Inherited from Token
+	virtual size_t id() const override;
+	virtual Token *copy() const override;
+	virtual type caller() const override;
+	virtual std::string dbg_str() const override;
+	virtual bool operator==(Token *) const override;
 };
 
 class MultiSet : public Token {
+public:
+	// Bool is for negation
+	using OpVec = std::vector <std::pair <Set *, bool>>;
 
+	enum class Operation {
+		Union,
+		Intersection
+	};
+private:
+	OpVec		_vec;
+	Operation	_op	= Union;
+public:
+	MultiSet(const OpVec &);
+
+	bool present(Token *) const;
+
+	// Inherited from Token
+	virtual size_t id() const override;
+	virtual Token *copy() const override;
+	virtual type caller() const override;
+	virtual std::string dbg_str() const override;
+	virtual bool operator==(Token *) const override;
 };
 
 class Generator : public Set {
-
+public:
+	
 };
 
 class Collection : public Generator {
