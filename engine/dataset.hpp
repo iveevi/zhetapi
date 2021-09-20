76ca4f35aa76897f8ea8923d3084aab67b89f72d
Venkataram Edavamadathil Sivaram <42660556+vedavamadathil@users.noreply.github.com>
2021-09-20 15:51:05 -0700

    Added asmjit library
    Also modified dataset header as a more convenient interface to datasets.

--- engine/dataset.hpp
+++ engine/dataset.hpp
@@ -18,9 +18,9 @@ template <class T>
 std::vector <DataSet <T>> split(const DataSet <T> &dset, size_t len)
 {
 	std::vector <DataSet <T>> batched;
-	
+
 	DataSet <T> batch;
-	
+
 	size_t size = dset.size();
 	for (int i = 0; i < size; i++) {
 		batch.push_back(dset[i]);
@@ -34,7 +34,44 @@ std::vector <DataSet <T>> split(const DataSet <T> &dset, size_t len)
 
 	return batched;
 }
-	
+
+// General sets of N-dimensional data
+template <class T, size_t N>
+class NumericalData {
+	// TODO: make more efficient?
+	Vector <T> _stddev() {
+		Vector <T> sum(N, 0);
+		for (const auto &vec : dataset) {
+			Vector <T> dx = (vec - _mean);
+			sum += shur(dx, dx);
+		}
+		return sum/(dataset.size() - _sample);
+	}
+
+	// TODO: fixed vector?
+	Vector <T> _mean() {
+		Vector <T> sum(N, 0);
+		for (const auto &vec : dataset)
+			sum += vec;
+		return sum/dataset.size();
+	}
+public:
+	DataSet		dataset;
+	Vector <T>	mean;
+	Vector <T>	stddev;
+	bool		sample;
+
+	// Sample indicates whether the dataset is a
+	// sample or the entire population
+	Data(const DataSet &set, bool sample = false)
+		: dataset(set), mean(_mean()),
+		stddev(_stddev()), sample(sample) {}
+};
+
+// Dimensional
+template <class T>
+using BivariateData <T> = NumericalData <T, 2>;
+
 }
 
 #else
