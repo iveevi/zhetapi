88aa487e06e4cc048fcac7738f7159e4a5feb41b
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2021-12-13 17:30:20 +0000

    More cleaning

--- engine/all/ml.hpp
+++
@@ -1,7 +0,0 @@
-// Combined header package for machine learning facilities
-#include "dnn.hpp"
-#include "training.hpp"
-
-#include "std/activations.hpp"
-#include "std/optimizers.hpp"
-#include "std/erfs.hpp"
--- engine/all/zhplib.hpp
+++
@@ -1,5 +0,0 @@
-// Combined header package for zhetapi-lang related facilties
-#include "../engine.hpp"
-#include "../cast.hpp"
-#include "../module.hpp"
-#include "../core/algorithm.hpp"
