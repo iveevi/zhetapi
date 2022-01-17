3b00d5382baf5d042514e28bb5f50276882cdc83
vedavamadathil <vesion4690@gmail.com>
2022-01-16 20:34:39 -0800

    Filling out derivatives for the otehr current function

--- source/autograd/autograd.cpp
+++ source/autograd/autograd.cpp
@@ -112,8 +112,10 @@ KERNEL(pow)
 DERIVATIVE(sqrt)
 {
 	ISeq *iseq = new ISeq();
+
+	// sqrt(f(x)) -> f'(x) / 2 * sqrt(f(x))
 	iseq->append(
-		_iop::differential(vindex),
+		_iop::differential(0),
 		new _repl_const(2.0, -1),
 		new Get(0),
 		new _sqrt::kernel(),
@@ -125,12 +127,108 @@ DERIVATIVE(sqrt)
 }
 
 // TODO: fill out the rest
-DERIVATIVE(exp) {return nullptr;}
-DERIVATIVE(log) {return nullptr;}
-DERIVATIVE(sin) {return nullptr;}
-DERIVATIVE(cos) {return nullptr;}
-DERIVATIVE(tan) {return nullptr;}
-DERIVATIVE(pow) {return nullptr;}
+DERIVATIVE(exp)
+{
+	ISeq *iseq = new ISeq();
+
+	// e^f(x) -> f'(x) * e^f(x)
+	iseq->append(
+		new Get(0),
+		new _exp::kernel(),
+		_iop::differential(0),
+		new _function(2, _function::op_mul)
+	);
+
+	return iseq;
+}
+
+DERIVATIVE(log)
+{
+	ISeq *iseq = new ISeq();
+
+	// log(f(x)) -> f'(x) / f(x)
+	iseq->append(
+		_iop::differential(0),
+		new Get(0),
+		new _log::kernel(),
+		new _function(2, _function::op_div)
+	);
+
+	return iseq;
+}
+
+DERIVATIVE(sin)
+{
+	ISeq *iseq = new ISeq();
+
+	// sin(f(x)) -> f'(x) * cos(f(x))
+	iseq->append(
+		_iop::differential(0),
+		new Get(0),
+		new _cos::kernel(),
+		new _function(2, _function::op_mul)
+	);
+
+	return iseq;
+}
+
+DERIVATIVE(cos)
+{
+	ISeq *iseq = new ISeq();
+
+	// cos(f(x)) -> -f'(x) * sin(f(x))
+	iseq->append(
+		_iop::differential(0),
+		new Get(0),
+		new _sin::kernel(),
+		new _function(2, _function::op_mul),
+		new _repl_const(-1, -1),
+		new _function(2, _function::op_mul)
+	);
+
+	return iseq;
+}
+
+// TODO: seq operation
+DERIVATIVE(tan)
+{
+	ISeq *iseq = new ISeq();
+
+	// tan(f(x)) -> f'(x) / (cos(f(x))^2)
+	iseq->append(
+		_iop::differential(0),
+		new Get(0),
+		new _cos::kernel(),
+		new Get(0),
+		new _cos::kernel(),
+		new _function(2, _function::op_mul),
+		_iop::differential(0),
+		new _function(2, _function::op_div)
+	);
+
+	return iseq;
+}
+
+// TODO: also the case where f(x)^g(x) -> check this!
+DERIVATIVE(pow)
+{
+	ISeq *iseq = new ISeq();
+
+	// TODO: gotta test all this
+
+	// f(x)^e -> e * f(x)^(e-1) * f'(x)
+	iseq->append(
+		_iop::differential(0),
+		new _repl_const(-1.0, -1),
+		new Get(1),
+		new _function(2, _function::op_sub),
+		new Get(0),
+		new _pow::kernel(),
+		new _function(2, _function::op_mul)
+	);
+
+	return iseq;
+}
 
 }
 
